5404cee47b18a14259f8aea8852d117c41a76d61
Konrad Dominowski <zehkan@gmail.com>
2023-09-17 17:40:17 +0200

    implemented optimistic updates for challenge

--- frontend/app/[slug]/challenge/[id]/Challenge/Main/Challenge.tsx
+++ frontend/app/[slug]/challenge/[id]/Challenge/Main/Challenge.tsx
@@ -2,13 +2,12 @@
 import React, { useEffect, useState } from "react";
 
 import { Session } from "next-auth";
-import { Box, Flex, IconButton, VisuallyHiddenInput, useToast } from "@chakra-ui/react";
+import { Box, Flex, IconButton, VisuallyHiddenInput } from "@chakra-ui/react";
 import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 
 import CodeEditor from "@/app/components/Editors";
 import TargetAndOutput from "@/app/components/TargetAndOutput";
 import { SubmitButton } from "@/app/components/Buttons";
-import { createCompleteStatus } from "../actions";
 
 import styles from "./Challenge.module.scss";
 
@@ -26,11 +25,9 @@ interface Props {
 // TODO - Kiedy się kliknie na opis, może się zwinąć
 // TODO - link do następnego i poprzedniego challengu kieruje na ID w bazie danych, na razie ono istnieje ale kiedyś mogą się pojebać, może zamiast id uży pola order
 export default function Challenge({ params, topic, taskData, session }: Props) {
-  const toast = useToast();
-  const [HTMLcode, setHTMLcode] = useState<string>(taskData?.htmlCode || "");
-  const [CSScode, setCSScode] = useState<string>(taskData?.cssCode || "");
+  const [HTMLcode, setHTMLcode] = useState<string>(taskData?.html_code || "");
+  const [CSScode, setCSScode] = useState<string>(taskData?.css_code || "");
   const [srcDoc, setSrcDoc] = useState<string>("");
-  const [notLoggedInInfoShown, setNotLoggedInInfoShown] = useState(false);
 
   const task = topic.chapters!.flatMap((chapter) => chapter.tasks).find((task) => task.id === +params.id);
 
@@ -47,28 +44,8 @@ export default function Challenge({ params, topic, taskData, session }: Props) {
     return () => clearTimeout(timeout);
   }, [HTMLcode, CSScode]);
 
-  async function onSubmitTask(formData: FormData) {
-    const res = await createCompleteStatus(formData, session);
-
-    if (res.status === "notLoggedIn") {
-      // TODO - save tasks data locally
-
-      if (!notLoggedInInfoShown) {
-        setNotLoggedInInfoShown(true);
-        toast({
-          position: "bottom-left",
-          title: "Sign in",
-          description: "Sign in to save data to your account, for now your data is saved locally",
-          status: "info",
-          duration: 12000,
-          isClosable: true,
-        });
-      }
-    }
-  }
-
   return (
-    <form action={onSubmitTask}>
+    <>
       <div className={styles.main} id="main">
         <Flex alignItems={"center"} justify={"space-between"}>
           <h1>{task!.title}</h1>
@@ -90,6 +67,6 @@ export default function Challenge({ params, topic, taskData, session }: Props) {
       <VisuallyHiddenInput type="number" name="task" readOnly value={params.id} />
       <VisuallyHiddenInput type="text" name="html_code" readOnly value={HTMLcode} />
       <VisuallyHiddenInput type="text" name="css_code" readOnly value={CSScode} />
-    </form>
+    </>
   );
 }
