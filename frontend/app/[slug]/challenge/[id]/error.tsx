73bf3f43c001564582f723940ecde104501c1780
Konrad Dominowski <zehkan@gmail.com>
2023-09-23 21:19:47 +0200

    changed to backend only repo

--- frontend/app/[slug]/challenge/[id]/error.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Error({ error }: { error: Error }) {
-  return <h1>There was an error fething this task</h1>;
-}
--- frontend/app/error.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export default function ErrorPage({ error, reset }: { error: Error; reset: () => void }) {
-  console.log(error);
-  return <h2>Error fetching data</h2>;
-}
