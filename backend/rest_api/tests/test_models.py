c2e158b7b770aaec929b8c58199347bf62632c4f
Konrad Dominowski <zehkan@gmail.com>
2024-01-18 01:38:56 +0100

    Add Task tests

--- backend/rest_api/tests/test_models.py
+++ backend/rest_api/tests/test_models.py
@@ -2,7 +2,7 @@ from django.db import IntegrityError
 from django.test import TestCase
 from django.utils.text import slugify
 
-from ..models import Topic, Chapter
+from ..models import Topic, Chapter, Task
 
 
 # python manage.py test
@@ -23,7 +23,6 @@ class TopicTestCase(TestCase):
                                             )
 
     def test_create_topic(self):
-        """Test if topic are added correctly"""
         self.assertEqual(Topic.objects.count(), 2)
 
         Topic.objects.create(title='Test topic 4',
@@ -35,11 +34,9 @@ class TopicTestCase(TestCase):
         self.assertEqual(Topic.objects.count(), 3)
 
     def test_topic_gets_slug(self):
-        """Test if slug is correctly created for the topic."""
         self.assertEqual(self.topic_1.slug, slugify(self.topic_1.title))
 
     def test_topic_are_ordered_correctly(self):
-        """Test if topic are ordered according to the value of order field in the db"""
         Topic.objects.create(title='Test topic 4',
                              logo_url='https://picsum.photos/200',
                              short_description='Test short description 4',
@@ -79,6 +76,69 @@ class TopicTestCase(TestCase):
 
 class ChapterTestCase(TestCase):
     def setUp(self):
+        self.topic_2 = Topic.objects.create(title='Test topic 2',
+                                            logo_url='https://picsum.photos/200',
+                                            short_description='Test short description 2',
+                                            long_description='Test long description 2',
+                                            order=2,
+                                            )
+        self.topic_1 = Topic.objects.create(title='Test topic 1',
+                                            logo_url='https://picsum.photos/200/300',
+                                            short_description='Test short description 1',
+                                            long_description='Test long description 1',
+                                            order=1,
+                                            )
+        self.chapter_1 = Chapter.objects.create(title='Test chapter 1',
+                                                topic=self.topic_1,
+                                                order=2)
+        self.chapter_2 = Chapter.objects.create(title='Test chapter 1',
+                                                topic=self.topic_1,
+                                                order=1)
+
+    def test_create_topic(self):
+        self.assertEqual(Chapter.objects.count(), 2)
+
+        Chapter.objects.create(title='Test chapter 3',
+                               topic=self.topic_1,
+                               order=3)
+        self.assertEqual(Chapter.objects.count(), 3)
+
+    def test_chapters_are_ordered_correctly(self):
+        Chapter.objects.create(title='Test chapter 6',
+                               topic=self.topic_2,
+                               order=2)
+        Chapter.objects.create(title='Test chapter 5',
+                               topic=self.topic_2,
+                               order=3)
+        Chapter.objects.create(title='Test chapter 4',
+                               topic=self.topic_2,
+                               order=1)
+        Chapter.objects.create(title='Test chapter 3',
+                               topic=self.topic_1,
+                               order=3)
+
+        queryset_1 = Chapter.objects.filter(topic_id=self.topic_1.id)
+        self.assertEqual(queryset_1[0].order, 1)
+        self.assertEqual(queryset_1[1].order, 2)
+        self.assertEqual(queryset_1[2].order, 3)
+
+        queryset_2 = Chapter.objects.filter(topic_id=self.topic_2.id)
+        self.assertEqual(queryset_2[0].order, 1)
+        self.assertEqual(queryset_2[1].order, 2)
+        self.assertEqual(queryset_2[2].order, 3)
+
+    def test_order_and_topic_is_unique(self):
+        with self.assertRaises(IntegrityError):
+            Chapter.objects.create(title='Test chapter 1',
+                                   topic=self.topic_1,
+                                   order=1)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.chapter_1), f'{self.chapter_1.topic} - {self.chapter_1.title}')
+
+
+class TaskTestCase(TestCase):
+    def setUp(self):
         self.topic_1 = Topic.objects.create(title='Test topic 1',
                                             logo_url='https://picsum.photos/200/300',
                                             short_description='Test short description 1',
@@ -91,38 +151,55 @@ class ChapterTestCase(TestCase):
         self.chapter_2 = Chapter.objects.create(title='Test chapter 1',
                                                 topic=self.topic_1,
                                                 order=1)
+        self.task_1 = Task.objects.create(title='Test task 1',
+                                          description="Test task description 1",
+                                          chapter=self.chapter_1,
+                                          order=2)
+        self.task_2 = Task.objects.create(title='Test task 2',
+                                          description="Test task description 2",
+                                          chapter=self.chapter_1,
+                                          order=1)
 
-    def test_create_topic(self):
-        """Test if chapters are added correctly"""
-        self.assertEqual(Chapter.objects.count(), 2)
+    def test_create_task(self):
+        self.assertEqual(Task.objects.count(), 2)
 
-        Chapter.objects.create(title='Test chapter 3',
-                               topic=self.topic_1,
-                               order=3)
-        self.assertEqual(Chapter.objects.count(), 3)
+        Task.objects.create(title='Test task',
+                            description="Test task description 2",
+                            chapter=self.chapter_1,
+                            order=3)
+        self.assertEqual(Task.objects.count(), 3)
 
-    def test_topic_are_ordered_correctly(self):
-        """Test if chapters are ordered according to the value of order field in the db"""
-        Chapter.objects.create(title='Test chapter 4',
-                               topic=self.topic_1,
-                               order=4)
-        Chapter.objects.create(title='Test chapter 3',
-                               topic=self.topic_1,
-                               order=3)
+    def test_tasks_are_ordered_correctly(self):
+        Task.objects.create(title='Test task',
+                            description="Test task description 1",
+                            chapter=self.chapter_2,
+                            order=2)
+        Task.objects.create(title='Test task',
+                            description="Test task description 1",
+                            chapter=self.chapter_2,
+                            order=1)
+        Task.objects.create(title='Test task',
+                            description="Test task description 2",
+                            chapter=self.chapter_1,
+                            order=3)
 
-        queryset = Chapter.objects.all()
+        tasks = Task.objects.filter(chapter__topic=self.topic_1)
+        tasks_in_chapter_1 = tasks.filter(chapter=self.chapter_1)
+        tasks_in_chapter_2 = tasks.filter(chapter=self.chapter_2)
 
-        self.assertEqual(queryset[0].order, 1)
-        self.assertEqual(queryset[1].order, 2)
-        self.assertEqual(queryset[2].order, 3)
-        self.assertEqual(queryset[3].order, 4)
+        self.assertEqual(tasks_in_chapter_1[0].order, 1)
+        self.assertEqual(tasks_in_chapter_1[1].order, 2)
+        self.assertEqual(tasks_in_chapter_1[2].order, 3)
 
-    def test_order_and_topic_is_unique(self):
-        """Test if chapter order is unique"""
+        self.assertEqual(tasks_in_chapter_2[0].order, 1)
+        self.assertEqual(tasks_in_chapter_2[1].order, 2)
+
+    def test_chapter_task_order_is_unique(self):
         with self.assertRaises(IntegrityError):
-            Chapter.objects.create(title='Test chapter 1',
-                                   topic=self.topic_1,
-                                   order=1)
+            Task.objects.create(title='Test task',
+                                description="Test task description 2",
+                                chapter=self.chapter_1,
+                                order=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.chapter_1), f'{self.chapter_1.topic} - {self.chapter_1.title}')
+        self.assertEqual(str(self.task_1), f'{self.task_1.chapter} - {self.task_1.title}')
