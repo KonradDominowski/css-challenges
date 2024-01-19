bc806eb30d545c352a03599be34411d7d0cacf1f
Konrad Dominowski <zehkan@gmail.com>
2024-01-19 01:01:23 +0100

    Add UserTask tests

--- backend/rest_api/tests/test_models.py
+++ backend/rest_api/tests/test_models.py
@@ -1,26 +1,67 @@
 from django.db import IntegrityError
 from django.test import TestCase
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-from ..models import Topic, Chapter, Task
+from ..models import Topic, Chapter, Task, UserTask
+
+
+def create_two_topics():
+    topic_2 = Topic.objects.create(title='Test topic 2',
+                                   logo_url='https://picsum.photos/200',
+                                   short_description='Test short description 2',
+                                   long_description='Test long description 2',
+                                   order=2,
+                                   )
+    topic_1 = Topic.objects.create(title='Test topic 1',
+                                   logo_url='https://picsum.photos/200/300',
+                                   short_description='Test short description 1',
+                                   long_description='Test long description 1',
+                                   order=1,
+                                   )
+
+    return topic_1, topic_2
+
+
+def create_two_chapters(topic: Topic):
+    chapter_1 = Chapter.objects.create(title='Test chapter 1',
+                                       topic=topic,
+                                       order=2)
+    chapter_2 = Chapter.objects.create(title='Test chapter 1',
+                                       topic=topic,
+                                       order=1)
+
+    return chapter_1, chapter_2
+
+
+def create_two_tasks(chapter: Chapter):
+    task_1 = Task.objects.create(title='Test task 1',
+                                 description="Test task description 1",
+                                 chapter=chapter,
+                                 order=2)
+    task_2 = Task.objects.create(title='Test task 2',
+                                 description="Test task description 2",
+                                 chapter=chapter,
+                                 order=1)
+
+    return task_1, task_2
+
+
+def create_two_users():
+    user_1 = User.objects.create_user(username='test_user_1',
+                                      email='test@test.com',
+                                      password='test onion')
+    user_2 = User.objects.create_user(username='test_user_2',
+                                      email='test@test.com',
+                                      password='test onion')
+
+    return user_1, user_2
 
 
 # python manage.py test
 class TopicTestCase(TestCase):
     def setUp(self):
-        # I create Test topic 2 first and then Test topic 1 to test if they are ordered correctly.
-        self.topic_2 = Topic.objects.create(title='Test topic 2',
-                                            logo_url='https://picsum.photos/200',
-                                            short_description='Test short description 2',
-                                            long_description='Test long description 2',
-                                            order=2,
-                                            )
-        self.topic_1 = Topic.objects.create(title='Test topic 1',
-                                            logo_url='https://picsum.photos/200/300',
-                                            short_description='Test short description 1',
-                                            long_description='Test long description 1',
-                                            order=1,
-                                            )
+        self.topic_1, self.topic_2 = create_two_topics()
 
     def test_create_topic(self):
         self.assertEqual(Topic.objects.count(), 2)
@@ -36,7 +77,7 @@ class TopicTestCase(TestCase):
     def test_topic_gets_slug(self):
         self.assertEqual(self.topic_1.slug, slugify(self.topic_1.title))
 
-    def test_topic_are_ordered_correctly(self):
+    def test_topics_are_ordered_correctly(self):
         Topic.objects.create(title='Test topic 4',
                              logo_url='https://picsum.photos/200',
                              short_description='Test short description 4',
@@ -76,24 +117,8 @@ class TopicTestCase(TestCase):
 
 class ChapterTestCase(TestCase):
     def setUp(self):
-        self.topic_2 = Topic.objects.create(title='Test topic 2',
-                                            logo_url='https://picsum.photos/200',
-                                            short_description='Test short description 2',
-                                            long_description='Test long description 2',
-                                            order=2,
-                                            )
-        self.topic_1 = Topic.objects.create(title='Test topic 1',
-                                            logo_url='https://picsum.photos/200/300',
-                                            short_description='Test short description 1',
-                                            long_description='Test long description 1',
-                                            order=1,
-                                            )
-        self.chapter_1 = Chapter.objects.create(title='Test chapter 1',
-                                                topic=self.topic_1,
-                                                order=2)
-        self.chapter_2 = Chapter.objects.create(title='Test chapter 1',
-                                                topic=self.topic_1,
-                                                order=1)
+        self.topic_1, self.topic_2 = create_two_topics()
+        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
 
     def test_create_topic(self):
         self.assertEqual(Chapter.objects.count(), 2)
@@ -139,26 +164,9 @@ class ChapterTestCase(TestCase):
 
 class TaskTestCase(TestCase):
     def setUp(self):
-        self.topic_1 = Topic.objects.create(title='Test topic 1',
-                                            logo_url='https://picsum.photos/200/300',
-                                            short_description='Test short description 1',
-                                            long_description='Test long description 1',
-                                            order='1',
-                                            )
-        self.chapter_1 = Chapter.objects.create(title='Test chapter 1',
-                                                topic=self.topic_1,
-                                                order=2)
-        self.chapter_2 = Chapter.objects.create(title='Test chapter 1',
-                                                topic=self.topic_1,
-                                                order=1)
-        self.task_1 = Task.objects.create(title='Test task 1',
-                                          description="Test task description 1",
-                                          chapter=self.chapter_1,
-                                          order=2)
-        self.task_2 = Task.objects.create(title='Test task 2',
-                                          description="Test task description 2",
-                                          chapter=self.chapter_1,
-                                          order=1)
+        self.topic_1, self.topic_2 = create_two_topics()
+        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
+        self.task_1, self.task_2 = create_two_tasks(self.chapter_1)
 
     def test_create_task(self):
         self.assertEqual(Task.objects.count(), 2)
@@ -203,3 +211,26 @@ class TaskTestCase(TestCase):
 
     def test_string_representation(self):
         self.assertEqual(str(self.task_1), f'{self.task_1.chapter} - {self.task_1.title}')
+
+
+class UserTaskTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = create_two_topics()
+        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
+        self.task_1, self.task_2 = create_two_tasks(self.chapter_1)
+        self.user_1, self.user_2 = create_two_users()
+        self.user_task_1 = UserTask.objects.create(task=self.task_1, user=self.user_1)
+        self.user_task_2 = UserTask.objects.create(task=self.task_2, user=self.user_1)
+
+    def test_create_user_task(self):
+        self.assertEqual(UserTask.objects.count(), 2)
+
+        UserTask.objects.create(task=self.task_2, user=self.user_2)
+        self.assertEqual(UserTask.objects.count(), 3)
+
+    def test_user_task_is_unique(self):
+        with self.assertRaises(IntegrityError):
+            UserTask.objects.create(task=self.task_1, user=self.user_1)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.user_task_1), f'{self.user_task_1.user} - {self.user_task_1.task}')
