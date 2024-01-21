1a178cf1db0c53ff5796e34787d4eb2f6c9c7700
Konrad Dominowski <zehkan@gmail.com>
2024-01-21 22:32:22 +0100

    Finished tests for models

--- backend/rest_api/tests/test_models.py
+++ backend/rest_api/tests/test_models.py
@@ -3,65 +3,75 @@ from django.test import TestCase
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
-from ..models import Topic, Chapter, Task, UserTask
-
-
-def create_two_topics():
-    topic_2 = Topic.objects.create(title='Test topic 2',
-                                   logo_url='https://picsum.photos/200',
-                                   short_description='Test short description 2',
-                                   long_description='Test long description 2',
-                                   order=2,
-                                   )
-    topic_1 = Topic.objects.create(title='Test topic 1',
-                                   logo_url='https://picsum.photos/200/300',
-                                   short_description='Test short description 1',
-                                   long_description='Test long description 1',
-                                   order=1,
-                                   )
-
-    return topic_1, topic_2
-
-
-def create_two_chapters(topic: Topic):
-    chapter_1 = Chapter.objects.create(title='Test chapter 1',
-                                       topic=topic,
-                                       order=2)
-    chapter_2 = Chapter.objects.create(title='Test chapter 1',
-                                       topic=topic,
-                                       order=1)
-
-    return chapter_1, chapter_2
-
-
-def create_two_tasks(chapter: Chapter):
-    task_1 = Task.objects.create(title='Test task 1',
-                                 description="Test task description 1",
-                                 chapter=chapter,
-                                 order=2)
-    task_2 = Task.objects.create(title='Test task 2',
-                                 description="Test task description 2",
-                                 chapter=chapter,
-                                 order=1)
-
-    return task_1, task_2
-
-
-def create_two_users():
-    user_1 = User.objects.create_user(username='test_user_1',
-                                      email='test@test.com',
-                                      password='test onion')
-    user_2 = User.objects.create_user(username='test_user_2',
-                                      email='test@test.com',
-                                      password='test onion')
-
-    return user_1, user_2
+from ..models import Topic, Chapter, Task, UserTask, TopicDescription, DescriptionBody, DescriptionParagraph, ToLearn, \
+    ToLearnItem
 
 
 # python manage.py test
+class Fixtures:
+    @staticmethod
+    def create_two_topics():
+        topic_2 = Topic.objects.create(title='Test topic 2',
+                                       logo_url='https://picsum.photos/200',
+                                       short_description='Test short description 2',
+                                       long_description='Test long description 2',
+                                       order=2,
+                                       )
+        topic_1 = Topic.objects.create(title='Test topic 1',
+                                       logo_url='https://picsum.photos/200/300',
+                                       short_description='Test short description 1',
+                                       long_description='Test long description 1',
+                                       order=1,
+                                       )
+
+        return topic_1, topic_2
+
+    @staticmethod
+    def create_two_chapters(topic: Topic):
+        chapter_1 = Chapter.objects.create(title='Test chapter 1',
+                                           topic=topic,
+                                           order=2)
+        chapter_2 = Chapter.objects.create(title='Test chapter 1',
+                                           topic=topic,
+                                           order=1)
+
+        return chapter_1, chapter_2
+
+    @staticmethod
+    def create_two_tasks(chapter: Chapter):
+        task_1 = Task.objects.create(title='Test task 1',
+                                     description="Test task description 1",
+                                     chapter=chapter,
+                                     order=2)
+        task_2 = Task.objects.create(title='Test task 2',
+                                     description="Test task description 2",
+                                     chapter=chapter,
+                                     order=1)
+
+        return task_1, task_2
+
+    @staticmethod
+    def create_two_users():
+        user_1 = User.objects.create_user(username='test_user_1',
+                                          email='test@test.com',
+                                          password='test onion')
+        user_2 = User.objects.create_user(username='test_user_2',
+                                          email='test@test.com',
+                                          password='test onion')
+
+        return user_1, user_2
+
+    @staticmethod
+    def create_topic_description(topic: Topic):
+        description_1 = TopicDescription.objects.create(topic=topic,
+                                                        subtitle='Test subtitle')
+
+        return description_1
+
+
 class TopicTestCase(TestCase):
     def setUp(self):
-        self.topic_1, self.topic_2 = create_two_topics()
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
 
     def test_create_topic(self):
         self.assertEqual(Topic.objects.count(), 2)
@@ -117,8 +127,8 @@ class TopicTestCase(TestCase):
 
 class ChapterTestCase(TestCase):
     def setUp(self):
-        self.topic_1, self.topic_2 = create_two_topics()
-        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.chapter_1, self.chapter_2 = Fixtures.create_two_chapters(self.topic_1)
 
     def test_create_topic(self):
         self.assertEqual(Chapter.objects.count(), 2)
@@ -164,9 +174,9 @@ class ChapterTestCase(TestCase):
 
 class TaskTestCase(TestCase):
     def setUp(self):
-        self.topic_1, self.topic_2 = create_two_topics()
-        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
-        self.task_1, self.task_2 = create_two_tasks(self.chapter_1)
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.chapter_1, self.chapter_2 = Fixtures.create_two_chapters(self.topic_1)
+        self.task_1, self.task_2 = Fixtures.create_two_tasks(self.chapter_1)
 
     def test_create_task(self):
         self.assertEqual(Task.objects.count(), 2)
@@ -215,10 +225,10 @@ class TaskTestCase(TestCase):
 
 class UserTaskTestCase(TestCase):
     def setUp(self):
-        self.topic_1, self.topic_2 = create_two_topics()
-        self.chapter_1, self.chapter_2 = create_two_chapters(self.topic_1)
-        self.task_1, self.task_2 = create_two_tasks(self.chapter_1)
-        self.user_1, self.user_2 = create_two_users()
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.chapter_1, self.chapter_2 = Fixtures.create_two_chapters(self.topic_1)
+        self.task_1, self.task_2 = Fixtures.create_two_tasks(self.chapter_1)
+        self.user_1, self.user_2 = Fixtures.create_two_users()
         self.user_task_1 = UserTask.objects.create(task=self.task_1, user=self.user_1)
         self.user_task_2 = UserTask.objects.create(task=self.task_2, user=self.user_1)
 
@@ -234,3 +244,97 @@ class UserTaskTestCase(TestCase):
 
     def test_string_representation(self):
         self.assertEqual(str(self.user_task_1), f'{self.user_task_1.user} - {self.user_task_1.task}')
+
+
+class TopicDescriptionTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.topic_description = Fixtures.create_topic_description(self.topic_1)
+
+    def test_create_topic_description(self):
+        self.assertEqual(TopicDescription.objects.count(), 1)
+
+        Fixtures.create_topic_description(self.topic_2)
+        self.assertEqual(TopicDescription.objects.count(), 2)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.topic_description), self.topic_description.topic.title)
+
+
+class DescriptionBodyTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.topic_description = Fixtures.create_topic_description(self.topic_1)
+        self.description_body = DescriptionBody.objects.create(topic_description=self.topic_description)
+
+    def test_create_topic_description(self):
+        self.assertEqual(DescriptionBody.objects.count(), 1)
+
+        self.topic_description_2 = Fixtures.create_topic_description(self.topic_2)
+        self.description_body_2 = DescriptionBody.objects.create(topic_description=self.topic_description_2)
+        self.assertEqual(DescriptionBody.objects.count(), 2)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.description_body), self.topic_description.topic.title)
+
+
+class DescriptionParagraphTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.topic_description = Fixtures.create_topic_description(self.topic_1)
+        self.description_body = DescriptionBody.objects.create(topic_description=self.topic_description)
+        self.paragraph_1 = DescriptionParagraph.objects.create(body=self.description_body,
+                                                               text="Test text")
+
+    def test_create_description_paragraph(self):
+        self.assertEqual(DescriptionParagraph.objects.count(), 1)
+
+        self.topic_description_2 = Fixtures.create_topic_description(self.topic_2)
+        self.description_body_2 = DescriptionBody.objects.create(topic_description=self.topic_description_2)
+        self.paragraph_1 = DescriptionParagraph.objects.create(body=self.description_body_2,
+                                                               text="Test text")
+        self.assertEqual(DescriptionParagraph.objects.count(), 2)
+
+    def test_string_representation(self):
+        (self.assertEqual
+         (str(self.paragraph_1), f'{self.paragraph_1.body.topic_description.topic.title} - {self.paragraph_1.pk}'))
+
+
+class ToLearnTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.topic_description = Fixtures.create_topic_description(self.topic_1)
+        self.to_learn_1 = ToLearn.objects.create(topic_description=self.topic_description)
+
+    def test_create_to_learn(self):
+        self.assertEqual(ToLearn.objects.count(), 1)
+
+        self.topic_description_2 = Fixtures.create_topic_description(self.topic_2)
+        self.to_learn_2 = ToLearn.objects.create(topic_description=self.topic_description_2)
+        self.assertEqual(ToLearn.objects.count(), 2)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.to_learn_1), self.to_learn_1.topic_description.topic.title)
+
+
+class ToLearnItemTestCase(TestCase):
+    def setUp(self):
+        self.topic_1, self.topic_2 = Fixtures.create_two_topics()
+        self.topic_description = Fixtures.create_topic_description(self.topic_1)
+        self.to_learn_1 = ToLearn.objects.create(topic_description=self.topic_description)
+        self.to_learn_item_1 = ToLearnItem.objects.create(to_learn=self.to_learn_1,
+                                                          main="Test main",
+                                                          sub="Test sub")
+
+    def test_create_to_learn_item(self):
+        self.assertEqual(ToLearnItem.objects.count(), 1)
+
+        self.To_learn_item_2 = ToLearnItem.objects.create(to_learn=self.to_learn_1,
+                                                          main="Test main",
+                                                          sub="Test sub")
+        self.assertEqual(ToLearnItem.objects.count(), 2)
+
+    def test_string_representation(self):
+        self.assertEqual(
+            str(self.to_learn_item_1),
+            f'{self.to_learn_item_1.to_learn.topic_description.topic.title} - {self.to_learn_item_1.pk}')
